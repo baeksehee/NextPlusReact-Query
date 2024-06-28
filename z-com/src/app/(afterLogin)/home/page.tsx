c8de1ef57184b042acd49d55901b926f2cae234d
Baek, Sehee <tpgmlqor@gmail.com>
2024-06-28 12:46:47 +0900

    Section03-react-query SSR 설정하기

--- z-com/src/app/(afterLogin)/home/page.tsx
+++ z-com/src/app/(afterLogin)/home/page.tsx
@@ -1,25 +1,56 @@
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 import Post from "../_component/Post";
 import PostForm from "./_component/PostForm";
 import Tab from "./_component/Tab";
 import TabProvider from "./_component/TabProvider";
 import style from "./home.module.css";
 
-export default function Home() {
+async function getPostRecommends() {
+  const res = await fetch("http://localhost:9090/api/postRecommends", {
+    next: {
+      tags: ["posts", "recommends"],
+    },
+  });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", "recommends"],
+    queryFn: getPostRecommends,
+  });
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <main className={style.main}>
-      <TabProvider>
-        <Tab />
-        <PostForm />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </TabProvider>
+      <HydrationBoundary state={dehydratedState}>
+        <TabProvider>
+          <Tab />
+          <PostForm />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </TabProvider>
+      </HydrationBoundary>
     </main>
   );
 }
