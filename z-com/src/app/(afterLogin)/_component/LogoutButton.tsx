09d6633fcd70ea9047905db40ac6d9b6d1559dcd
Baek, Sehee <tpgmlqor@gmail.com>
2024-07-14 10:37:45 +0900

    Section04-완전히 로그아웃하기(1)

--- z-com/src/app/(afterLogin)/_component/LogoutButton.tsx
+++ z-com/src/app/(afterLogin)/_component/LogoutButton.tsx
@@ -1,17 +1,30 @@
 "use client";
 
-import { Session } from "@auth/core/types";
 import style from "./logoutButton.module.css";
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { Session } from "@auth/core/types";
+import { useQueryClient } from "@tanstack/react-query";
 
-type Props = { me: Session | null };
-
+type Props = {
+  me: Session | null;
+};
 export default function LogoutButton({ me }: Props) {
   const router = useRouter();
+  const queryClient = useQueryClient();
 
   const onLogout = () => {
+    queryClient.invalidateQueries({
+      queryKey: ["posts"],
+    });
+    queryClient.invalidateQueries({
+      queryKey: ["users"],
+    });
     signOut({ redirect: false }).then(() => {
+      fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/logout`, {
+        method: "post",
+        credentials: "include",
+      });
       router.replace("/");
     });
   };
